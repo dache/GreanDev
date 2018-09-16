6983aaf3f27b9baf6ee6ea1f997ddb8b9ac971ab
indieos <wittawus.s@gmail.com>
2018-09-16 04:14:39 -0700

    no message

--- ExclusiveGym.WinForms/Models/StorageManager.cs
+++ ExclusiveGym.WinForms/Models/StorageManager.cs
@@ -181,13 +181,29 @@ class StorageManager
 
         SaveDB();
     }
+
     public void GetIncomeToday()
     {
-
+        
     }
 
     public void GetMemberAccessGymToday()
     {
 
+    }
+
+    public List<ApplyCourseLog> GetIncomeByDay(int day, int month, int year)
+    {
+        return GetDB().ApplyCourseLog.Where(f => f.ApplyDate.Date == new DateTime(year, month, day)).ToList();
+    }
+
+    public List<ApplyCourseLog> GetIncomeByMonth(int month, int year)
+    {
+        return GetDB().ApplyCourseLog.Where(f => f.ApplyDate.Month == month && f.ApplyDate.Year == year ).ToList();
+    }
+
+    public List<ApplyCourseLog> GetIncomeByYear(int year)
+    {
+        return GetDB().ApplyCourseLog.Where(f => f.ApplyDate.Year == year).ToList();
     }
 }
